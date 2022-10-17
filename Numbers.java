5ca848ca1310b47c952ba963a5249af47976ea78
lmacbride <L00177577@atu.ie>
2022-10-17 19:50:00 +0100

    Added Numbers.java file

---
+++ Numbers.java
@@ -0,0 +1,26 @@
+package numbers;
+
+public class Numbers {
+
+	public static void sum(){
+		
+	}
+	
+	public static void subtract(){
+	
+	}
+	
+	public static void largest(){
+		
+	}
+
+	public static void smallest(){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
