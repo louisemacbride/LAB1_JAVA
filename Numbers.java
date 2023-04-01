cf0d92dde6055986f811db39fd86512e854f968e
louisemacbride <113636388+louisemacbride@users.noreply.github.com>
2023-04-01 23:02:33 +0100

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -1,3 +1,17 @@
-public int calculateTotal(int quantity, int price) {
-    return quantity * price;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+    }
 }
