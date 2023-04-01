a0f744655d30ca50624233407e7979a93f708cd6
louisemacbride <113636388+louisemacbride@users.noreply.github.com>
2023-04-01 23:11:24 +0100

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -15,9 +15,35 @@ public class Calculator {
         int sum = num1 + num2;
 
         System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+        
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+        
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+   
+    
+    
+    
     }
     
-    public int add(int a, int b) {
-            return a + b;
-           }
+           
 }
