8e079b8bc41e2b121df12b849c878017765ef582
louisemacbride <113636388+louisemacbride@users.noreply.github.com>
2023-04-01 17:52:32 +0100

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -27,5 +27,38 @@ public class Calculator {
 
         sr = new SecureRandom("abcdefghijklmnop".getBytes("us-ascii")); // Noncompliant
         v = sr.next(32);
+        
+                System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+        
+        int sum2 = sum + sum;
+        System.out.println("The sum of " + sum + " and " + sum + " is " + sum3 + ".");
+        
+        String password = "mypassword";
+        System.out.println("The password is: " + password1);
+        
+        
+                System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+        
+        int sum2 = sum + sum;
+        System.out.println("The sum of " + sum + " and " + sum + " is " + sum3 + ".");
+        
+        String password = "mypassword";
+        System.out.println("The password is: " + password1);
     }
 }
