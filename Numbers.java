dd6063a78eb3223bcccb34810b6c1fee0c76260e
louisemacbride <113636388+louisemacbride@users.noreply.github.com>
2023-04-01 22:53:54 +0100

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -1,26 +1,3 @@
-import java.util.Scanner;
-
-public class Calculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
-
-        int sum = num1 + num2;
-
-        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
-        
-        int sum2 = sum + sum;
-        System.out.println("The sum of " + sum + " and " + sum + " is " + sum3 + ".");
-        
-        String password = "mypassword";
-        System.out.println("The password is: " + password1);
-        
-     
-    }
-
+public int calculateTotal(int quantity, int price) {
+    return quantity * price;
 }
